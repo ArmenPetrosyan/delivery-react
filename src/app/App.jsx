a165a30a169aeb3a12e543c4dc0b57e5c30ddd61
arnie <arnie@templatemonster.me>
2016-12-25 13:58:45 +0200

    ADD: icons

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -22,7 +22,7 @@ export default class App extends Component {
   }
 
   state = {
-
+    orders: []
   };
 
   render () {
@@ -32,45 +32,92 @@ export default class App extends Component {
     };
 
     const about = (
-      <p className="f1 text-center spacer-top-80 padding-bottom-40 padding-both-160">
+      <p className="dashes f1 text-center padding-top-40 padding-bottom-40 padding-both-160">
         На карте отображены заявки на доставку товаров с указанием пунктов назначения, ценой и весом груза.
         Для получения дополнительной информации щелкните на значек автомобиля.
       </p>
     );
 
-    const newOrderForm = (
-      <Form>
-        <FormGroup>
-          <h3>
-            Список заказов
-          </h3>
+    const emptyList = (
+      <div className="dark-theme text-center">
+        <img src="svg/ghost-2.svg" alt=""/>
+        <p className="ghost-p bold spacer-top-20">Список заказов пуст</p>
+        <Button bsStyle="success">Создать заказ</Button>
+      </div>
+    );
+
+    let activeOrders = (
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Откуда</th>
+            <th>Куда</th>
+            <th>О</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+          </tr>
+        </thead>
+      </Table>
+      this.state.orders.map((current) => {
+          return (
+
+          );
+      })
+    );
+
+    const ordersForm = (
+      <Form horizontal>
+        <FormGroup controlId="formFrom">
+          <Col sm={3}>
+            <ControlLabel>
+              Откуда
+            </ControlLabel>
+            <FormControl type="email" placeholder="Email" />
+          </Col>
         </FormGroup>
       </Form>
     );
 
+    const ordersList = (
+      <div>
+        <h3 className="padding-bottom-40">
+          Список заказов
+        </h3>
+        { !this.state.orders.length ? emptyList : activeOrders }
+        {/*{ ordersForm }*/}
+      </div>
+    );
+
     const interactiveMap = (
-      <Gmaps
-        width={'100%'}
-        height={'600px'}
-        lat={coords.lat}
-        lng={coords.lng}
-        zoom={14}
-        loadingMessage={'Be happy'}
-        params={{v: '3.exp'}}
-        onMapCreated={this.onMapCreated}>
-        <Marker
+      <div className="padding-top-80">
+        { about }
+        <Gmaps
+          width={'100%'}
+          height={'600px'}
           lat={coords.lat}
           lng={coords.lng}
-          draggable={true}
-          onDragEnd={this.onDragEnd}
-          icon = 'src/app/business.svg'
-        />
-        <InfoWindow
-          lat={coords.lat}
-          lng={coords.lng}
-          content={'Основной Склад'}
-          onCloseClick={this.onCloseClick} />
-      </Gmaps>
+          zoom={14}
+          loadingMessage={'Be happy'}
+          params={{v: '3.exp'}}
+          onMapCreated={this.onMapCreated}>
+          <Marker
+            lat={coords.lat}
+            lng={coords.lng}
+            draggable={true}
+            content={'Основной Склад'}
+            onDragEnd={this.onDragEnd}
+            icon = 'svg/trolley.svg'
+          />
+          <InfoWindow
+            lat={coords.lat}
+            lng={coords.lng}
+            content={'Основной Склад'}
+            onCloseClick={this.onCloseClick} />
+        </Gmaps>
+      </div>
+
     );
     return (
       <article>
@@ -81,10 +128,9 @@ export default class App extends Component {
             </h1>
           </Grid>
         </header>
-        <main className="dark-theme service-header">
+        <main className="dark-theme service-header padding-bottom-40">
           <Grid>
-            { newOrderForm }
-            { about }
+            { ordersList }
             { interactiveMap }
           </Grid>
         </main>
